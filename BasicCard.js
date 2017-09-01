5a13265838a269eb4cb2e320b3e5d7cf15fff69f
Kelcey Asuao <kelceyasuao@gmail.com>
2017-09-01 08:15:44 -0600

    BasicCard.js about half done

---
+++ BasicCard.js
@@ -0,0 +1,37 @@
+//create the constructor
+//pass the parameters front and back
+function BasicCard(front, back){
+	this.front = front;
+	this.back = back;
+};
+
+var term1 = new BasicCard(
+	"a variable that contains a list", "array");
+
+var term2 = new BasicCard(
+	'it is a "holding place" where values are stored', "variable");
+
+var term3 = new BasicCard(
+	'recipe of instructions in JavaScript', 'function');
+
+var term4 = new BasicCard(
+	'an aggregation of variables and functions', 'object');
+
+var term5 = new BasicCard(
+	'variables that make up an object', 'properties');
+
+var term6 = new BasicCard(
+	'the function of a property in an object also called a member function', 'method');
+
+var term7 = new BasicCard(
+	'a predefined "schema" that can be used to create an object later', 'constructor');
+
+var term8 = new BasicCard(
+	'performs a function on one or more operands or variables', 'operator');
+
+var term9 = new BasicCard(
+	'an html attribute that assign a JS function when an event happens', 'event handler');
+
+var term10 = new BasicCard(
+	'lines in the code that are usually there to describe what the code is doing', 'comments');
+
